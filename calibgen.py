648e22695fcb4b1d70ba01c95300336bfabc298f
Friðfinnur M <notandinotandi@gmail.com>
2020-02-21 09:27:36 +0000

    Calibgen generates a thread profile with many size offsets (e.g. +0.1, +0.2, +0.3) for desiging thread fit test coupons.

---
+++ calibgen.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+* Modifies a Fusion 360 thread profile to contain classes of
+* threads that are shifted larger by steps, to aid in calibration.
+"""
+
+from lxml import etree
+from io import open
+
+#threadlibpath = "./testdata.xml"
+threadlibpath = "./ISOMetricprofile.xml"
+outfile = "./ISOMetricprofileCalibration.xml"
+
+with open(threadlibpath, "br") as f:
+    threadlib = etree.parse(f)
+
+threads = threadlib.getroot().iterfind(".//ThreadSize")
+for ts in threads:
+    size = float(ts.find(".//Size").text)
+    for designation in ts.iterfind(".//Designation"):
+        adjthreads = []  # Buffer additions to the designation to avoid reprocessing
+        for thread in designation.iterfind(".//Thread"):
+            if thread.find(".//Gender").text == "external":  # Don't adjust external threads
+                continue
+            # x is [0.1, 0.2, 0.3 etc.]
+            for adjustment in [x/100 for x in range(10, 100+1, 10)]:
+                tclass = thread.find(".//Class").text + \
+                    "{:+.2f}".format(adjustment)
+                majordia = round(
+                    float(thread.find(".//MajorDia").text) + adjustment, 3)
+                pitchdia = round(
+                    float(thread.find(".//PitchDia").text) + adjustment, 3)
+                minordia = round(
+                    float(thread.find(".//MinorDia").text) + adjustment, 3)
+
+                adjthread = \
+                    etree.XML(
+                        "<Thread>\n"
+                        "   <Gender>internal</Gender>\n"
+                        "   <Class>{}</Class>\n"
+                        "   <MajorDia>{}</MajorDia>\n"
+                        "   <PitchDia>{}</PitchDia>\n"
+                        "   <MinorDia>{}</MinorDia>\n"
+                        "</Thread>\n".format(tclass, majordia, pitchdia, minordia))
+                adjthreads.append(adjthread)
+        for t in adjthreads:
+            designation.append(t)
+
+etree.indent(threadlib)
+with open(outfile, "wb") as f:
+    threadlib.write(f, pretty_print=True)
--- threadmod.py
+++ threadmod.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from lxml import etree
 from os import path
 from io import open
@@ -7,10 +8,10 @@ import matplotlib.pyplot as plt
 import functools
 
 
-#todo: locate fusion360 installation
+# todo: locate fusion360 installation
 
 #threadlibpath = "./testdata.xml"
-threadlibpath = "./ISOMetricProfile.xml"
+threadlibpath = "./ISOMetricprofile.xml"
 outfile = "./ISOMetricProfileAdjusted.xml"
 
 classprefix = "UM2 "
@@ -19,31 +20,35 @@ with open(threadlibpath, "br") as f:
     threadlib = etree.parse(f)
 
 
-threadsizes = list(map(lambda e: float(e.text), threadlib.getroot().findall(".//ThreadSize/Size")))
+threadsizes = list(map(lambda e: float(e.text),
+                       threadlib.getroot().findall(".//ThreadSize/Size")))
 print(threadsizes)
 
-datapoints = [(3.0,0.58), (5.0, 0.55), (8.0, 0.5), (12.0, 0.4), (50, 0)]
+datapoints = [(3.0, 0.58), (5.0, 0.55), (8.0, 0.5), (12.0, 0.4), (50, 0)]
 xs = np.linspace(min(threadsizes), max(threadsizes))
 
-def sigmoid(p,x):
-    x0,y0,c,k=p
+
+def sigmoid(p, x):
+    x0, y0, c, k = p
     y = c / (1 + np.exp(-k*(x-x0))) + y0
     return y
 
-def residuals(p,x,y):
-    return y - sigmoid(p,x)
 
-x,y = zip(*datapoints)
+def residuals(p, x, y):
+    return y - sigmoid(p, x)
+
+
+x, y = zip(*datapoints)
 p_guess = (np.median(x), np.median(y), 1.0, 1.0)
 p, cov, infodict, mesg, ier = scipy.optimize.leastsq(
-    residuals, p_guess, args=(x,y), full_output=True
+    residuals, p_guess, args=(x, y), full_output=True
 )
 
-xp = np.linspace(0,50,500)
-pxp =  np.maximum(0,sigmoid(p, xp))
+xp = np.linspace(0, 50, 500)
+pxp = np.maximum(0, sigmoid(p, xp))
 
 
-plt.plot(x,y,'.', xp,pxp,'-')
+plt.plot(x, y, '.', xp, pxp, '-')
 plt.show()
 
 adjfun = functools.partial(sigmoid, p)
@@ -51,20 +56,23 @@ adjfun = functools.partial(sigmoid, p)
 threads = threadlib.getroot().iterfind(".//ThreadSize")
 for ts in threads:
     size = float(ts.find(".//Size").text)
-    adjustment = round(adjfun(size),3)
+    adjustment = round(adjfun(size), 3)
     print(adjustment)
     for designation in ts.iterfind(".//Designation"):
         print(designation.find(".//ThreadDesignation").text)
         adjthreads = []
         for thread in designation.iterfind(".//Thread"):
             print(thread.find(".//Class").text)
-            if thread.find(".//Gender").text == "external": # Don't adjust external threads
+            if thread.find(".//Gender").text == "external":  # Don't adjust external threads
                 continue
 
             tclass = classprefix + thread.find(".//Class").text
-            majordia = round(float(thread.find(".//MajorDia").text) + adjustment, 3)
-            pitchdia = round(float(thread.find(".//PitchDia").text) + adjustment, 3)
-            minordia = round(float(thread.find(".//MinorDia").text) + adjustment, 3)
+            majordia = round(
+                float(thread.find(".//MajorDia").text) + adjustment, 3)
+            pitchdia = round(
+                float(thread.find(".//PitchDia").text) + adjustment, 3)
+            minordia = round(
+                float(thread.find(".//MinorDia").text) + adjustment, 3)
 
             adjthread = \
                 etree.XML(
@@ -74,7 +82,7 @@ for ts in threads:
                     "   <MajorDia>{}</MajorDia>\n"
                     "   <PitchDia>{}</PitchDia>\n"
                     "   <MinorDia>{}</MinorDia>\n"
-                    "</Thread>\n".format(tclass,majordia,pitchdia,minordia))
+                    "</Thread>\n".format(tclass, majordia, pitchdia, minordia))
             adjthreads.append(adjthread)
         print(adjthreads)
         for t in adjthreads:
@@ -85,6 +93,3 @@ for ts in threads:
 etree.indent(threadlib)
 with open(outfile, "wb") as f:
     threadlib.write(f, pretty_print=True)
-
-
-
